9306c6f1f3f864967bf1a15149f5fa663b8b5105
misk <misksawallha@gmail.com>
2023-03-05 09:05:01 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,176 @@
+let card = document.getElementById('card');
+let products = {
+    "results": [
+        {
+            "productID": "KL2432",
+            "productName": "Hugo Boss men's suit",
+            "productPrice": 2500,
+            "productPriceFormatted": "DKK 2.500,00",
+            "inStock": true,
+            "productImg": "https://akamai-scene7.garnethill.com/is/image/garnethill/49187_SADD?&defaultImage=49187_main",
+            "href": "/riley-heeled-buckle-boots/shoes-accessories/new-arrivals/572383?uniqueId=572383",
+            "swatches": [
+                {
+                    "swatchDefAttCode": "BLCK",
+                    "swatchName": "Black",
+                    "swatchAttrName": "color",
+                    "img": { "src": "https://akamai-scene7.garnethill.com/is/image/garnethill/48611_BLCK_SW?$SLI_MiniSwatch$" }
+                }
+            ]
+        },
+        {
+            "productID": "KL2432",
+            "productName": "Hugo Boss men's suit",
+            "productPrice": 2500,
+            "productPriceFormatted": "DKK 2.500,00",
+            "inStock": true,
+            "productImg": "https://akamai-scene7.garnethill.com/is/image/garnethill/49187_SADD?&defaultImage=49187_main",
+            "href": "/riley-heeled-buckle-boots/shoes-accessories/new-arrivals/572383?uniqueId=572383",
+            "swatches": [
+                {
+                    "swatchDefAttCode": "SADD",
+                    "swatchName": "Distressed Saddle",
+                    "swatchAttrName": "color",
+                    "img": { "src": "https://akamai-scene7.garnethill.com/is/image/garnethill/49187_SADD_SW?$SLI_MiniSwatch$" }
+                },
+                {
+                    "swatchDefAttCode": "Mahogany",
+                    "swatchName": "Mahogany",
+                    "swatchAttrName": "color",
+                    "img": { "src": "https://akamai-scene7.garnethill.com/is/image/garnethill/49187_MAHO_SW?$SLI_MiniSwatch$" }
+                }
+            ]
+        },
+        {
+            "productID": "KL2432",
+            "productName": "Hugo Boss men's suit",
+            "productPrice": 2500,
+            "productPriceFormatted": "DKK 2.500,00",
+            "inStock": true,
+            "productImg": "https://akamai-scene7.garnethill.com/is/image/garnethill/49187_SADD?&defaultImage=49187_main",
+            "href": "/riley-heeled-buckle-boots/shoes-accessories/new-arrivals/572383?uniqueId=572383",
+            "swatches": [
+                {
+                    "swatchDefAttCode": "SADD",
+                    "swatchName": "Distressed Saddle",
+                    "swatchAttrName": "color",
+                    "img": { "src": "https://akamai-scene7.garnethill.com/is/image/garnethill/49187_SADD_SW?$SLI_MiniSwatch$" }
+                },
+                {
+                    "swatchDefAttCode": "Mahogany",
+                    "swatchName": "Mahogany",
+                    "swatchAttrName": "color",
+                    "img": { "src": "https://akamai-scene7.garnethill.com/is/image/garnethill/49187_MAHO_SW?$SLI_MiniSwatch$" }
+                },{
+                    "swatchDefAttCode": "BLCK",
+                    "swatchName": "Black",
+                    "swatchAttrName": "color",
+                    "img": { "src": "https://akamai-scene7.garnethill.com/is/image/garnethill/48611_BLCK_SW?$SLI_MiniSwatch$" }
+                }
+            ]
+        },
+        {
+            "productID": "KL2432",
+            "productName": "Hugo Boss men's suit",
+            "productPrice": 2500,
+            "productPriceFormatted": "DKK 2.500,00",
+            "inStock": true,
+            "productImg": "https://akamai-scene7.garnethill.com/is/image/garnethill/49187_SADD?&defaultImage=49187_main",
+            "href": "/riley-heeled-buckle-boots/shoes-accessories/new-arrivals/572383?uniqueId=572383",
+            "swatches": [
+                {
+                    "swatchDefAttCode": "SADD",
+                    "swatchName": "Distressed Saddle",
+                    "swatchAttrName": "color",
+                    "img": { "src": "https://akamai-scene7.garnethill.com/is/image/garnethill/49187_SADD_SW?$SLI_MiniSwatch$" }
+                },
+                {
+                    "swatchDefAttCode": "Mahogany",
+                    "swatchName": "Mahogany",
+                    "swatchAttrName": "color",
+                    "img": { "src": "https://akamai-scene7.garnethill.com/is/image/garnethill/49187_MAHO_SW?$SLI_MiniSwatch$" }
+                }
+            ]
+        },
+        {
+            "productID": "KL2432",
+            "productName": "Hugo Boss men's suit",
+            "productPrice": 2500,
+            "productPriceFormatted": "DKK 2.500,00",
+            "inStock": true,
+            "productImg": "https://akamai-scene7.garnethill.com/is/image/garnethill/49187_SADD?&defaultImage=49187_main",
+            "href": "/riley-heeled-buckle-boots/shoes-accessories/new-arrivals/572383?uniqueId=572383",
+            "swatches": [
+                {
+                    "swatchDefAttCode": "SADD",
+                    "swatchName": "Distressed Saddle",
+                    "swatchAttrName": "color",
+                    "img": { "src": "https://akamai-scene7.garnethill.com/is/image/garnethill/49187_SADD_SW?$SLI_MiniSwatch$" }
+                },
+                {
+                    "swatchDefAttCode": "Mahogany",
+                    "swatchName": "Mahogany",
+                    "swatchAttrName": "color",
+                    "img": { "src": "https://akamai-scene7.garnethill.com/is/image/garnethill/49187_MAHO_SW?$SLI_MiniSwatch$" }
+                }
+            ]
+        },
+        {
+            "productID": "KL2432",
+            "productName": "Hugo Boss men's suit",
+            "productPrice": 2500,
+            "productPriceFormatted": "DKK 2.500,00",
+            "inStock": true,
+            "productImg": "https://akamai-scene7.garnethill.com/is/image/garnethill/49187_SADD?&defaultImage=49187_main",
+            "href": "/riley-heeled-buckle-boots/shoes-accessories/new-arrivals/572383?uniqueId=572383",
+            "swatches": [
+                {
+                    "swatchDefAttCode": "SADD",
+                    "swatchName": "Distressed Saddle",
+                    "swatchAttrName": "color",
+                    "img": { "src": "https://akamai-scene7.garnethill.com/is/image/garnethill/49187_SADD_SW?$SLI_MiniSwatch$" }
+                },
+                {
+                    "swatchDefAttCode": "Mahogany",
+                    "swatchName": "Mahogany",
+                    "swatchAttrName": "color",
+                    "img": { "src": "https://akamai-scene7.garnethill.com/is/image/garnethill/49187_MAHO_SW?$SLI_MiniSwatch$" }
+                }
+            ]
+        }
+    ]
+};
+
+displayData();
+
+function displayData(){
+    let res = ``;
+    let len = products.results.length;
+  
+    for(let i = 0 ; i < len ; i++){
+        let swatchRes = ``;
+        let lent = products.results[i].swatches.length;
+        for(let j = 0 ; j < lent ; j++){
+            swatchRes += `
+            <div class="small-items">
+                    <img src=${products.results[i].swatches[j].img.src}>
+            </div>
+            `;
+        }
+        res+=`
+        <div class="card-items">
+            <img class="image" src=${products.results[i].productImg}>
+            
+            <div class="small-items">
+            <p>${products.results[i].productName}</p>
+                ${swatchRes}
+                <p>${products.results[i].productPrice}$</p>
+            <p>${products.results[i].productPriceFormatted}</p>
+            </div>
+            
+        </div>
+        `
+    }
+    card.innerHTML = res;
+}
+
